4112eaf0d4683a402a20cfeb0903793185af3515
Johnny <55157591+xkaDachi@users.noreply.github.com>
2020-10-15 21:38:27 -0400

    Update Q4.lisp
    works 100%

--- Q4.lisp
+++ Q4.lisp
@@ -4,7 +4,5 @@
         ((and (not (null (car (car (cdr aList))))) (< (car aList) (car (car (cdr aList))))) nil)
         ((and (not (null (car (car (cdr (cdr aList)))))) (> (car aList) (car (car (cdr (cdr aList)))))) nil)
         (t (and (binary-search (car (cdr aList))) (binary-search (car (cdr aList)))))))
-    
-
-
+   
 (print(binary-search '(8 (3 (1 () ()) (6 (4 () ())( 7 () ()))) (10 (()) (14 (13) ())))))

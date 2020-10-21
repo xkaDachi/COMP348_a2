98ea651ab130321438f8dda3943534b2b2cff6ef
Johnny <55157591+xkaDachi@users.noreply.github.com>
2020-10-21 19:15:22 -0400

    Update Q1.lisp
    updated with minor changes

--- Q1.lisp
+++ Q1.lisp
@@ -1,35 +1,24 @@
 ;sub-list function
 (defun sub-list (listInput fromIndex toIndex)
-  (if (or (> fromIndex (myLength listInput)) (> toIndex (myLength listInput))) nil ;indexes cannot be out of bounds
-    (assist-sub-list listInput fromIndex toIndex 1)
-  )
-)
-
+  (if (or (> fromIndex (myLength listInput)) (> toIndex (myLength listInput))) nil  ;indexes cannot be out of bounds 
+    (assist-sub-list listInput fromIndex toIndex 1))) ;helper function               & calling this here because myLength will change in each recursive calls
+    
 ;user-defined length function
 (defun myLength (aList)
   (if aList
     (+ 1 (myLength (cdr aList)))
-    0
-  )
-)
-
+    0))
+  
 ;helper function
 (defun assist-sub-list (listInput fromIndex toIndex position)
   (if (or (< fromIndex 1) (< toIndex 1)) nil ;indexes can't be negative 
-    ;(if (or (> fromIndex (myLength listInput)) (> toIndex (myLength listInput))) nil ;indexes cannot be out of bounds
       (if (> fromIndex toIndex) nil ;fromIndex can't be higher than toIndex
          (if (< position fromIndex) ;ignores until we reach fromIndex
-              (assist-sub-list (cdr listInput) fromIndex toIndex (+ position 1))
-              (if (<= position toIndex) ;construct a list from fromIndex to toIndex
-                 (cons (car listInput) (assist-sub-list (cdr listInput) fromIndex toIndex (+ position 1))) 
-             )
-          )
-      )
-    )
- ;)
-)
+             (assist-sub-list (cdr listInput) fromIndex toIndex (+ position 1))
+             (if (<= position toIndex) ;construct a list from fromIndex to toIndex
+                (cons (car listInput) (assist-sub-list (cdr listInput) fromIndex toIndex (+ position 1)))))))) ;recursive call
 
-
+;test cases
 (print (sub-list '(1 6 12) 2 3)) ;from assignment
 (print (sub-list '(1 6 12) 1 2)) ;from assignment
 (print (sub-list '(1 6 12) 1 3)) ;from assignment

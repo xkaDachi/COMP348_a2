36f1ec83716b89c3f177890326fef91d9f759097
Johnny <55157591+xkaDachi@users.noreply.github.com>
2020-10-22 13:04:46 -0400

    Update and rename Q8.txt to Q8.lisp
    ask Ali what sequence does the Lucas sequence start at.
    0 or 1

---
+++ Q8.lisp
@@ -0,0 +1,17 @@
+(defun lucas (n &optional (a 2) (b 1))
+  (if (minusp n) ;Lucas Sequence starts at '0'. If starts at '1', use (or (zerop n) (minusp n)) instead
+      nil
+      (cons a (lucas (1- n) b (+ a b))))) ;construct a list recursively
+
+(print (lucas -1))
+(print (lucas 0))
+(print (lucas 1))
+(print (lucas 2))
+(print (lucas 3))
+(print (lucas 4))
+(print (lucas 5))
+(print (lucas 6))
+(print (lucas 7))
+(print (lucas 8))
+(print (lucas 9))
+(print (lucas 10))

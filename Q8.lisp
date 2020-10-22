6f74e7f76d88b76c40f9fd2f0b5bc410bc2f4870
Johnny <55157591+xkaDachi@users.noreply.github.com>
2020-10-22 13:10:58 -0400

    Update Q8.lisp

--- Q8.lisp
+++ Q8.lisp
@@ -1,7 +1,7 @@
 (defun lucas (n &optional (a 2) (b 1))
-  (if (minusp n) ;Lucas Sequence starts at '0'. If starts at '1', use (or (zerop n) (minusp n)) instead
+  (if (minusp n) ;Lucas Sequence starts at '0'. ***If starts at '1', use (or (zerop n) (minusp n)) instead
       nil
-      (cons a (lucas (1- n) b (+ a b))))) ;construct a list recursively
+      (cons a (lucas (- n 1) b (+ a b))))) ;construct a list recursively   ***(1- n) equivalent to (- n 1)
 
 (print (lucas -1))
 (print (lucas 0))

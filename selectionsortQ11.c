6e7c8b2ff2963027000c22f64a1fe44a6f3457c0
Johnny <55157591+xkaDachi@users.noreply.github.com>
2020-11-08 15:40:17 -0500

    Add files via upload
    updated selectionsortQ11

--- selectionsortQ11.c
+++ selectionsortQ11.c
@@ -12,16 +12,31 @@ int* selectionsortQ11(int* array, int size, int* (*findminPtr)(int* arr, int siz
     int indexCurrent = 0;
     int originalSize = size;
 
+    //If it's a NULL function pointer
     if (findminPtr == 0) {
-        return minPtr = findmin(array, size);
-    }
-    else {
         while (size != 0) {
 
             minPtr = findmin(array, size);
 
             for (int i = 0; i < size; i++) {
                 if (&array[i] == minPtr) {
+                    indexMin = i;
+                }
+            }
+            temp = array[indexCurrent];
+            array[indexCurrent] = array[indexMin];
+            array[indexMin] = temp;
+            array = array + 1;
+            size--;
+        }
+        return array = array - originalSize;
+    }
+    //else is not a NULL function pointer
+    else {
+        while (size != 0) {
+
+            for (int i = 0; i < size; i++) {
+                if (&array[i] == findminPtr) {
                     indexMin = i;
                 }
             }

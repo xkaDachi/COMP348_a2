18c7ade9f7f091cf1ba277ba262c09d01a1bd2ba
Johnny <55157591+xkaDachi@users.noreply.github.com>
2020-11-08 13:41:02 -0500

    Delete Q11.h

--- Q11.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/*
-* findMin declaration.
-* receives a third argument, a pointer
-* to a "min" function, that is to be called by the sort implementation
-*/
-int* selectionSortQ11(int* array, int size, int* (*findminPtr)(int* arr, int size));

a3059cc8dfa115894871dcfb7f9dd029b7c89a17
Johnny <55157591+xkaDachi@users.noreply.github.com>
2020-11-11 20:21:38 -0500

    Add files via upload
    modified Q13

--- Driver.c
+++ Driver.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "selectionsort.h" //#include "selectionsort.h"
 #include "selectionsort.h" //#include "selectionsort.h" // included twice
-#include "Q13.h"
+
+#define QUIT "." //end the loop call with user input as '.'
+#define EOF "EOF" //end the loop call with user input as ''EOF''
+#define SIZE 255
 
 //									 --------------------------
-//									 |  FUNCTION DECLARATIONS |
+//									 |     Q9 DECLARATIONS    |
 //									 --------------------------
 /*
 * findmin declaration for Q9
 */
 int* findminQ9(int* arr, int size);
 
-//										-----------------
-//										| MAIN FUNCTION |
-//										-----------------
+
+//									 --------------------------
+//									 |     Q13 DECLARATIONS   |
+//									 --------------------------
+/*
+* Struct Node to be used for ''LinkedList'' behavior
+*/
+struct Node {
+	char data[SIZE];
+	struct Node* next;
+};
+
+//Giving Node a new name
+typedef struct Node node;
+typedef struct Node* NodePointer;
+
+//Initializing Linked list to be empty
+NodePointer head = NULL;
+
+//Function declarations
+void insert_dictionary_order(char string[]);
+void print_list();
+
+
+//									    	-----------------
+//									    	| MAIN FUNCTION |
+//									    	-----------------
 //----------------------------------------------------------------------------------------------------l
 int main() {
     
@@ -92,8 +120,8 @@ int main() {
 	   strcpy(input, word);
 	   char* token = strtok(word, " "); // Extract the first token
 	   while (token != NULL) { // loop through the string to extract all other tokens
-		   insert_dictionary_order(token, &head);
-		   print_list(head);
+		   insert_dictionary_order(token);
+		   print_list();
 		   token = strtok(NULL, " ");
 	   }
    }//end of while
@@ -101,9 +129,9 @@ int main() {
 }
 //----------------------------------------------------------------------------------------------------
 
-//								 --------------------------
-//								 |  FUNCTION DEFINITIONS  |
-//								 --------------------------
+//								 ------------------------------
+//								 |  Q9 FUNCTION DEFINITIONS   |
+//								 ------------------------------
 /*
 * findminQ9 definition, TO SHOW FOR DEMO
 * Q10 and onwards will use the one defined in selection.c (Internal)
@@ -121,3 +149,70 @@ int* findminQ9(int* arr, int size) {
 	}
 	return smallestAddress;
 }
+
+//								 ------------------------------
+//								 |  Q13 FUNCTION DEFINITIONS  |
+//								 ------------------------------
+/*
+Receives a word (of type char*)
+Inserts is into the right position and in alphabetical order
+wordArray: is a string
+head: is the a pointer to the 1st node in a linked list
+This parameter is a pointer to a pointer because we are passing the address of the linked list to the function,
+and the linked list itself is a pointer to the head node in the list.
+*/
+void insert_dictionary_order(char string[]) {
+    //pointer to a new node to be inserted into linked list
+    NodePointer newNode = NULL;
+    //pointer to the previous node in the linked list
+    NodePointer previous = NULL;
+    //pointer to the current node in the linked list
+    NodePointer current = head;
+
+    //create a node on the heap
+    newNode = malloc(sizeof(node));
+    /*
+    check to see if space is available
+    if no space on heap, malloc() will return NULL
+    */
+    if (NULL != newNode) {
+        //copy over word to the node
+        strcpy(newNode->data, string);
+        //figure out where to insert in linked list
+        while (NULL != current && strcmp(string, current->data) > 0) {
+            //move previous to current
+            previous = current;
+            //move current to next node
+            current = current->next;
+        }//end of while
+        //insert at beginning of linked list
+        if (NULL == previous) {
+            newNode->next = current;
+            //change the address stored in head
+            head = newNode;
+        }//end of if
+        else {
+            //insert between previous and current
+            previous->next = newNode;
+            newNode->next = current;
+        }//end of else
+    }//end of if
+}//end of function
+
+//prints the dictionary that is a linkedlist
+void print_list() {
+    //for empty list
+    if (NULL == head) {
+        printf("The Dictionary is empty!\n\n");
+        return;
+    }
+    printf("Dictionary is the following: ");
+    //loop through linkedlist
+    while (NULL != head) {
+        //display each node
+        printf("%s -> ", head->data);
+        //go to next node
+        head = head->next;
+    }
+    printf("\n\n");
+}//end of function
